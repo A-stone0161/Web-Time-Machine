44213c8ff5a62a8557dbd7ae4b6d700b3eadb2d2
nimaldon <nicolem_1020@icloud.com>
2025-06-09 16:24:22 -0700

    fruitiger aero and evil images added as background, continuation on theme and info

--- 2025/js/lab.js
+++ 2025/js/lab.js
@@ -3,7 +3,7 @@
 // Date:
 
 // Add a button to each special-sections
-$(".theme").append("<button class='button-special'>Change Theme</button>");
+$().append("<button class='button-special'>Change Theme</button>");
 // Add a click listener for each Make Special button
 $(".theme .button-special").click(function() {
     // add or remove the special class from its parent section.
